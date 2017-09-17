5ffca528fe3ce5971da78d6dafd3fd8b1a2a8080
gsharm5u <gauravsati19@gmail.com>
2017-09-17 17:46:31 +0530

    0.33 Reached

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,41 +2,73 @@
 
 import sys
 import pickle
+import matplotlib.pyplot
+
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary','bonus','shared_receipt_with_poi','total_stock_value','from_this_person_to_poi','from_poi_to_this_person'] # You will need to use more features
+email_features = ['fraction_from_poi_email','fraction_to_poi_email']
+identifier_feature = ['poi']
+finance_features = []
+features_list = identifier_feature+email_features+finance_features # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 
+
+
 ### Task 2: Remove outliers
-# data = featureFormat(data_dict, features_list)
-# import matplotlib.pyplot
-#
-# for point in data:
-#     salary = point[0]
-#     bonus = point[1]
-#     matplotlib.pyplot.scatter( salary, bonus )
-#
-# matplotlib.pyplot.xlabel("salary")
-# matplotlib.pyplot.ylabel("bonus")
-# matplotlib.pyplot.show()
 data_dict.pop('TOTAL',0)
 
+
 ### Task 3: Create new feature(s)
+### new features are: fraction_to_poi_email,fraction_from_poi_email
+
+def dict_to_list(key,normalizer):
+    new_list=[]
+
+    for i in data_dict:
+        if data_dict[i][key]=="NaN" or data_dict[i][normalizer]=="NaN":
+            new_list.append(0.)
+        elif data_dict[i][key]>=0:
+            new_list.append(float(data_dict[i][key])/float(data_dict[i][normalizer]))
+    return new_list
+
+### create two lists of new features
+fraction_from_poi_email=dict_to_list("from_poi_to_this_person","to_messages")
+fraction_to_poi_email=dict_to_list("from_this_person_to_poi","from_messages")
+
+### insert new features into data_dict
+count=0
+for i in data_dict:
+    data_dict[i]["fraction_from_poi_email"]=fraction_from_poi_email[count]
+    data_dict[i]["fraction_to_poi_email"]=fraction_to_poi_email[count]
+    count +=1
+
+
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+for point in data:
+    to_poi = point[1]
+    from_poi = point[2]
+    matplotlib.pyplot.scatter( to_poi, from_poi )
+
+matplotlib.pyplot.xlabel("from_this_person_to_poi")
+matplotlib.pyplot.ylabel("from_poi_to_this_person")
+matplotlib.pyplot.show()
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -49,33 +81,38 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn import tree
 
-clf = GaussianNB()
-# clf=clf = SVC(kernel="rbf",C=10)
-# clf = tree.DecisionTreeClassifier(min_samples_split=40)
+# clf = GaussianNB()
+# clf = SVC(kernel="rbf",C=100)
+clf = tree.DecisionTreeClassifier(min_samples_split=20)
 
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 
+
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+
 clf.fit(features_train,labels_train)
+
+
+
+
 
 res = []
 for i in range(0,len(features_test)):
-    res.append(clf.predict(features_test[i]))
+    res.append(clf.predict(features_test[i])[0])
 print labels_test
 print res
-
 
 print precision_score(labels_test,res)
 print recall_score(labels_test,res)
